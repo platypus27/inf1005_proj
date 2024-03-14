0afe0aa0f0e1accd2e9efeb2230903aa824d40a1
Ao Yu <84365804+platypus27@users.noreply.github.com>
2024-03-15 00:32:22 +0800

    update login page

--- app/view/login.php
+++ app/view/login.php
@@ -1,13 +1,13 @@
 <section class="card border-0 m-3" id="login">
-    <h1>Member Login</h1>
+    <h1 style="color:#ffc2c3;">login</h1>
     <form class="login text-left" name="frmLogin" action="/login/login_process" method="POST">
         <div class="form-group">
-            <label for="loginid">Login ID</label>
-            <input class="form-control" type="text" required id="loginid" name="loginid" placeholder="Enter Login ID">
+            <label for="loginid">user</label>
+            <input class="form-control" type="text" required id="loginid" name="loginid">
         </div>
         <div class="form-group">
-            <label for="password">Password:</label>
-            <input class="form-control" type="password" id="password" required name="password" placeholder="Enter password">
+            <label for="password">password</label>
+            <input class="form-control" type="password" id="password" required name="password">
             <br>
             <div class="form-group m-auto">
                 <input type="hidden" name="<?=FORM_CSRF_FIELD?>" value="<?= $_SESSION[SESSION_CSRF_TOKEN]; ?>">
