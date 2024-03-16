df1617d04087f29b705617931f77aa0cfb8bb6e7
Ao Yu <84365804+platypus27@users.noreply.github.com>
2024-03-16 15:53:45 +0800

    should be done

--- app/view/main.php
+++ app/view/main.php
@@ -2,13 +2,8 @@
 
 <?php if(isset($_GET['timeout'])):
 if ($_GET['timeout']):?>
-    <article class="card border-0 intro-head p-5">
-        <div class="alert alert-danger alert-dismissible fade show mt-2 mb-0 alert-box comment-alert" role="alert">
-            <h5 class="text-center">Your Session has timeout!! Please sign in again</h5>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+    <article class="card border-0 intro-head p-5 container">
+        <h5 class="text-center">session timeout please sign in again</h5>
     </article>
 <?php endif;
 endif; ?>
